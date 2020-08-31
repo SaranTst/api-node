a0f4a93cd9d0dda4d1fa3d20fb8fb05e466d4a1f
Mark <saran_tst@hotmail.com>
2020-08-31 18:42:15 +0700

    custom chek sanitizeBody

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const AuthController = require("../controllers/AuthController");
 const router = express.Router(); //express.Router() => สร้าง router object
 
 router.post("/register", AuthController.register);
+router.post("/register2", AuthController.register2); // test
 router.post("/login", AuthController.login);
 router.post("/verify-otp", AuthController.verifyConfirm);
 router.post("/resend-verify-otp", AuthController.resendConfirmOtp);
